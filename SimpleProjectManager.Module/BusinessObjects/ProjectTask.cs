2724e1a12264d37e44f2b5e5bb0b99d167a07c55
Teo <Teo@DESKTOP-7JUSON1>
2024-11-10 22:17:59 +0200

    Add project files.

---
+++ SimpleProjectManager.Module/BusinessObjects/ProjectTask.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.Persistent.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleProjectManager.Module.BusinessObjects
+{
+	[NavigationItem("Planning")]
+	[RuleCriteria("EndDate >= StartDate",
+	CustomMessageTemplate = "Start Date must be less than End Date")]
+	[Appearance("InProgress", TargetItems = "Subject;AssignedTo",
+	Criteria = "Status = 1", BackColor = "LemonChiffon")]
+	public class ProjectTask : BaseObject
+	{
+		[FieldSize(255)]
+		public virtual string Subject { get; set; }
+
+		public virtual ProjectTaskStatus Status { get; set; }
+
+		public virtual Employee AssignedTo { get; set; }
+
+		public virtual DateTime? DueDate { get; set; }
+
+		public virtual DateTime? StartDate { get; set; }
+
+		public virtual DateTime? EndDate { get; set; }
+
+		[StringLength(4096)]
+		public virtual string Notes { get; set; }
+
+		public virtual Project Project { get; set; }
+
+	}
+
+	public enum ProjectTaskStatus
+	{
+		NotStarted = 0,
+		InProgress = 1,
+		Completed = 2,
+		Deferred = 3
+	}
+}
